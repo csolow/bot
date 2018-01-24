ed247e54e3607752524e9af71718fe881b57ef09
csolow <conradssolow@gmail.com>
2018-01-24 19:24:08 +0100

    First commit

---
+++ bot/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace bot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 45);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 94);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(148, 45);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 94);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(174, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "output";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "State:Awake";
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 199);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Bot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+    }
+}
+
---
+++ bot/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Xml;
+using System.IO;
+
+namespace bot
+{
+    public partial class Form1 : Form
+    {
+
+        SpeechSynthesizer s = new SpeechSynthesizer();
+        Boolean wake = true;
+        SerialPort port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+        String temp;
+        String condition;
+        String high;
+        String low;
+        Choices list = new Choices();
+        public Form1()
+        {
+
+            SpeechRecognitionEngine rec = new SpeechRecognitionEngine();
+
+
+            list.Add(new String[] {"Hello", "how are you", "what time is it", "what is today", "open google", "thanks", "wake", "sleep", "restart", "update", "open hearts of iron 4", "what's the weather like", "what's the temperature",
+                "what is lowest point today", "what is higest point today", "minimize", "maximize", "play", "pause", "spotify", "next", "last" });
+
+            Grammar gr = new Grammar(new GrammarBuilder(list));
+
+
+            try
+            {
+
+                rec.RequestRecognizerUpdate();
+                rec.LoadGrammar(gr);
+                rec.SpeechRecognized += rec_SpeachRecognized;
+                rec.SetInputToDefaultAudioDevice();
+                rec.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch { return; }
+
+            s.SelectVoiceByHints(VoiceGender.Female);
+
+            s.Speak("hello, my name is nanna");
+
+            InitializeComponent();
+        }
+        public String GetWeather(String input)
+        {
+            String query = String.Format("https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='copenhagen, state')&format=xml&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
+            XmlDocument wData = new XmlDocument();
+            try
+            {
+                wData.Load(query);
+            }
+            catch
+            {
+                MessageBox.Show("No internet connection");
+                return "No internet";
+            }
+
+            XmlNamespaceManager manager = new XmlNamespaceManager(wData.NameTable);
+            manager.AddNamespace("yweather", "http://xml.weather.yahoo.com/ns/rss/1.0");
+
+            XmlNode channel = wData.SelectSingleNode("query").SelectSingleNode("results").SelectSingleNode("channel");
+            XmlNodeList nodes = wData.SelectNodes("query/results/channel");
+            try
+            {
+                int rawtemp = int.Parse(channel.SelectSingleNode("item").SelectSingleNode("yweather:condition", manager).Attributes["temp"].Value);
+                temp = (rawtemp - 32) * 5/9 + "";
+                condition = channel.SelectSingleNode("item").SelectSingleNode("yweather:condition", manager).Attributes["text"].Value;
+                high = channel.SelectSingleNode("item").SelectSingleNode("yweather:forecast", manager).Attributes["high"].Value;
+                low = channel.SelectSingleNode("item").SelectSingleNode("yweather:forecast", manager).Attributes["low"].Value;
+                if (input == "temp")
+                {
+                    return temp; 
+                }
+                if (input == "high")
+                {
+                    return high;
+                }
+                if (input == "low")
+                {
+                    return low;
+                }
+                if (input == "cond")
+                {
+                    return condition;
+                }
+            }
+            catch
+            {
+                return "Error Reciving data";
+            }
+            return "error";
+        }
+
+        public void restart()
+        {
+            Process.Start(@"C:\Users\bot\bot.exe");
+            Environment.Exit(0);
+        }
+
+        public void say(String h)
+        {
+            s.Speak(h);
+            wake = true;
+            textBox2.AppendText(h + "\n");
+        }
+
+        private void rec_SpeachRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            String r = e.Result.Text;
+
+            if (r == "wake")
+            {
+                wake = true;
+                label3.Text = "State:Awake";
+                say("wake mode is now active");
+            }
+
+
+            if (r == "sleep")
+            {
+                wake = false;
+                label3.Text = "State:Sleep mode";
+                say("sleep mode is now active");
+            }
+
+        
+
+            if (wake == true)
+            {
+
+                if (r == "open hearts of iron 4")
+                {
+                    Process.Start(@"C:\Program Files (x86)\Steam\steamapps\common\Hearts of Iron IV\hoi4");
+                    say("for the people, everything");
+                }
+
+                if (r == "spotify")
+                {
+                    Process.Start(@"C:\Users\cswag\Desktop\Spotify");
+                    say("somebody once told me the world was gonna roll me i aint the sharpest tool in the shed, she was looking kind of dumb with her finger and her thumb in the shape of an L on her forehead");
+                }
+
+                if (r == "next")
+                {
+                    SendKeys.Send("^{RIGHT}");
+                }
+
+                if (r == "last")
+                {
+                    SendKeys.Send("^{LEFT}");
+                }
+
+                if (r == "play" || r == "pause")
+                {
+                    SendKeys.Send(" ");
+                }
+
+                if (r == "restart" || r == "update")
+                {
+                    restart();
+                }
+
+                if (r == "what is today")
+                {
+                    say(DateTime.Now.ToString("d/M/yyyy"));
+                }
+
+                if (r == "Hello")
+                {
+                    say("hi");
+                }
+
+                if (r == "what's the weather like")
+                {
+                    say("The sky is " + GetWeather("cond") + ",");
+                }
+
+                if (r == "what's the temperature")
+                {
+                    say("it is " + GetWeather("temp") + "degrees");
+                }
+
+                if (r == "what is higest point today")
+                {
+                    say("the higest point today is " + GetWeather("high") + "degrees");
+                }
+
+                if (r == "what is lowest point today")
+                {
+                    say("the lowest point today is " + GetWeather("low") + "degrees");
+                }
+
+                if (r == "how are you")
+                {
+                    say("great, and you?");
+                }
+
+                if (r == "what time is it")
+                {
+                    say(DateTime.Now.ToString("h:m tt"));
+                }
+
+                if (r == "what is today")
+                {
+                    say(DateTime.Now.ToString("d/M/yyyy"));
+                }
+
+                if (r == "minimize")
+                {
+                    this.WindowState = FormWindowState.Minimized;
+                }
+
+                if (r == "maximize")
+                {
+                    this.WindowState = FormWindowState.Normal;
+                }
+
+                if (r == "open google")
+                {
+                    Process.Start("https://google.com");
+                }
+
+                if (r == "thanks")
+                {
+                    say("you're welcome");
+                }
+            }
+            textBox1.AppendText(r + "\n");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
